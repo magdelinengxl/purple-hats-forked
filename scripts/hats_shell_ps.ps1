43676a2e031791e14325319322bb6300a411032a
magdelinengxl <magdelinenxl@gmail.com>
2022-12-16 16:02:12 +0800

    edit win

--- scripts/hats_shell_ps.ps1
+++ scripts/hats_shell_ps.ps1
@@ -14,10 +14,10 @@ echo "INFO: Set path to node_modules for this session"
 $env:Path = "$path_to_hats\node_modules\.bin;./node_modules/.bin;$env:Path";
 
 echo "INFO: Set path to npm-global for this session"
-$env:Path = "$env:Path;$path_to_hats\npm-global;$path_to_hats\npm-global\bin";
+$env:Path = "$path_to_hats\npm-global;$path_to_hats\npm-global\bin;$env:Path";
 
 echo "INFO: Set path to purple-hats for this session"
-$env:Path = "$env:Path;$path_to_hats\purple-hats"
+$env:Path = "$path_to_hats\purple-hats;$env:Path"
  
 echo ""
 $allArgs = $PsBoundParameters.Values + $args + ""
