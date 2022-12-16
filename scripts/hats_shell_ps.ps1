edfc0a7e43db3401273cfff4d2b081cb9ceed23e
magdelinengxl <magdelinenxl@gmail.com>
2022-12-16 14:58:30 +0800

    edit win

--- scripts/hats_shell_ps.ps1
+++ scripts/hats_shell_ps.ps1
@@ -11,7 +11,7 @@ echo "INFO: Set path to node for this session"
 $env:Path = "$env:Path;$path_to_hats\nodejs-win";
 
 echo "INFO: Set path to node_modules for this session"
-$env:Path = "$env:Path;$path_to_hats\node_modules\.bin;./node_modules/.bin";
+$env:Path = "$env:Path;$path_to_hats\purple-hats\node_modules\.bin;./purple-hats/node_modules/.bin";
 
 echo "INFO: Set path to npm-global for this session"
 $env:Path = "$env:Path;$path_to_hats\npm-global;$path_to_hats\npm-global\bin";
